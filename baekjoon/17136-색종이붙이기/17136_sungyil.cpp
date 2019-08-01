625933233ed11119b855ea25d51a7b73045274d1
SungYil <32151744+SungYil@users.noreply.github.com>
2019-08-01 23:07:35 +0900

    update sungyil 17136 file - success
    1. dfs 들어갈때마다 for문순회 방식이 아닌 1을 찾자마자 덮는 방식
    2. 순회방식은 한자리에만 머뭄 . 한자리에서 해결이 안되면 무한으로 돌거나 이상한 답 도출.

--- baekjoon/17136-색종이붙이기/17136_sungyil.cpp
+++ baekjoon/17136-색종이붙이기/17136_sungyil.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-void dfsF(int paper[10][10], int bang, int curCnt, int curLeft, int colorP[5]);
+void dfsFin(int paper[10][10], int curCnt, int curLeft, int colorP[5]);
 
 int paper[10][10] = { 0, };
 int colorP[5] = { 5,5,5,5,5 };
@@ -16,7 +16,7 @@ int main() {
 		}
 	}
 
-	dfsF(paper,5,0,leftPaper,colorP);
+	dfsFin(paper, 0, leftPaper, colorP);
 
 	if (cnt != 3200) {
 		cout << cnt << endl;
@@ -27,56 +27,61 @@ int main() {
 	getchar();
 	getchar();
 }
-void dfsF(int paper[10][10], int bang, int curCnt, int curLeft,int colorP[5]) {
-	if (bang == 0) {//마지막에 다다랏다면
+void dfsFin(int paper[10][10], int curCnt, int curLeft, int colorP[5]) {
+	if (curCnt > cnt)return;//종이수 많으면 포기
+	if (curLeft <= 0) {//모두 붙였다면
+		if (curCnt < cnt) {
+			
+			cnt = curCnt;
+		}
+	}
+	int x, y,t;
 
-		if (curLeft <= 0) {
-			if (cnt > curCnt) {
-				for (int i = 0; i < 5; ++i) {
-					cout<<colorP[i]<<" ";
-				}
-				cout << endl;
-				cnt = curCnt;
+	for (y=0; y < 10; ++y) {
+		t = 0;
+		for (x = 0; x < 10; ++x) {
+			if (paper[y][x] == 1) {
+				t = 1;
+				break;
 			}
 		}
-		return;
+		if (t==1)break;
 	}
-	int newPaper[10][10] = { 0, };
-	int newColorP[5] = { 0, };
-	memcpy(newPaper, paper, 10 * 10*sizeof(int));
-	memcpy(newColorP, colorP, 5*sizeof(int));
-	dfsF(newPaper, bang - 1, curCnt, curLeft, newColorP);//현재방에서 0장 덮고나서 다음방.
-	for (int i = 0; i < 10; ++i) {//1장씩 증가해서 다음방
-		for (int j = 0; j < 10; ++j) {
-			
-			if (paper[i][j] == 1&&colorP[bang-1]>0) {
-				int che = 0;
-				for (int h = 0; h <bang; ++h) {//덮을 가로 탐색
-					if (h + i >= 10)break;
-					for (int k = 0; k < bang; ++k) {
-						if (k + j >= 10)break;
-						che += paper[i + h][j + k];
-					}
+	//cout << "x=" << x << " y=" << y<<" "<<paper[y][x] << endl;
+	for (int bang = 5; bang > 0; --bang) {
+		if (colorP[bang - 1] > 0) {
+			bool che = true;
+			for (int i = 0; i < bang; ++i) {
+				if (i + y >= 10) {
+					che = false;
+					break;
 				}
-				if (che == bang * bang) {// 다덮는다면
-					for (int h = 0; h < bang; ++h) {//덮어 버린다.
-						if (h + i >= 10)break;
-						for (int k = 0; k < bang; ++k) {
-							if (k + j >= 10)break;
-							paper[i + h][j + k] = 0;
-						}
+				for (int j = 0; j < bang; ++j) {
+					if (j + x >= 10) {
+						che = false;
+						break;
 					}
-					curCnt++;
-					colorP[bang-1]--;
-					curLeft -= bang * bang;
-					int newPaper[10][10] = { 0, };
-					int newColorP[5] = { 0, };
-					memcpy(newPaper, paper, 10 * 10*sizeof(int));
-					memcpy(newColorP, colorP, 5*sizeof(int));
-					dfsF(newPaper, bang - 1, curCnt, curLeft, newColorP);//큰거 한장 덮었음 ㄱㄱ
+					if (paper[y + i][x + j] == 0) {
+						che = false; break;
+					}
 				}
 			}
 
+			if (che) {
+				int newPaper[10][10] = { 0, };
+				int newColorP[5] = { 0, };
+				memcpy(newPaper, paper, 10 * 10 * sizeof(int));
+				memcpy(newColorP, colorP, 5 * sizeof(int));
+				for (int i = 0; i < bang; ++i) {
+					//if (i + y >= 10)break;
+					for (int j = 0; j < bang; ++j) {
+						//if (j + x >= 10)break;
+						newPaper[y+i][x+j] = 0;
+					}
+				}
+				newColorP[bang - 1]--;
+				dfsFin(newPaper, curCnt + 1, curLeft - (bang*bang),newColorP );
+			}
 		}
 	}
 
