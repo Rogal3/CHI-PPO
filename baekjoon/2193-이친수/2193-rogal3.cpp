10bc580452d4f0b500f492a14076aef3031c1679
Rogal3 <52327048+Rogal3@users.noreply.github.com>
2019-08-02 08:24:21 +0900

    Create 2193-rogal3.cpp
    A형에 절대 안나오는 유형

---
+++ baekjoon/2193-이친수/2193-rogal3.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+long long dp[91][2];
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    dp[1][1] = 1;
+    dp[2][0] = 1;
+    for (int i = 3; i <= N; ++i) {
+        dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+        dp[i][1] = dp[i - 1][0];
+    }
+    printf("%lld", dp[N][0] + dp[N][1]);
+    return 0;
+}
