01347bc52b7363f79896971c28f06f54ccb17fdc
SungYil <32151744+SungYil@users.noreply.github.com>
2019-09-05 03:09:11 +0900

    Update 13460_sungyil.cpp
    https://jason9319.tistory.com/217 얘꺼 공부해서 한건데 잘안나옴 ;;
    다시 제대로 공부해서 해볼것

--- baekjoon/13460_구슬탈출/13460_sungyil.cpp
+++ baekjoon/13460_구슬탈출/13460_sungyil.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-#include <utility>
+#include <cstring>
+#include <queue>
 using namespace std;
-void findss(char bmap[11][11], pair<int, int> red, pair<int, int> blue, int dept);
-int* movingCheck(char bmap[11][11], pair<int, int> red);
-int movingBall(char bmap[11][11], pair<int, int> red, pair<int, int> blue, int moving);
+void bfs();
 
+struct Node{
+	int rx,ry,bx,by;
+};
 int n, m;
 char bmap[11][11];
 pair<int, int> spoted[3];
 
 int moveY[4] = { -1,1,0,0 };
 int moveX[4] = { 0,0,-1,1 };
-
+int cheking[11][11][11][11];
 bool tcheck = true;
 int ans = -1;
 int main() {
@@ -21,89 +23,78 @@ int main() {
 		for (int j = 0; j < m; ++j) {
 			cin >> bmap[i][j];
 			if (bmap[i][j] == 'R') {
-				spoted[0] = make_pair(i, j);
+				spoted[0] = make_pair(j, i);
 			}
 			else if (bmap[i][j] == 'B') {
-				spoted[2] = make_pair(i, j);
+				spoted[2] = make_pair(j, i);
 			}
 		}
 	}
-	findss(bmap,spoted[0], spoted[2], 1);
+
+	bfs();
 
 	cout << ans;
 
 	getchar();
 	getchar();
 }
-void findss(char bmap[11][11],pair<int, int> red, pair<int, int> blue, int dept) {
-	if (dept == 11) {
-		return;
-	}
-	if (tcheck) {
-		int* move = movingCheck(bmap, red);
+void bfs(){
+	queue<Node> q;
+	Node n={spoted[0].first,spoted[0].second,spoted[2].first,spoted[2].second};
+	cout<<n.rx<<n.ry<<n.bx<<n.by<<endl;
+	q.push(n);
+	int dept=0;
+	cheking[spoted[0].first][spoted[0].second][spoted[2].first][spoted[2].second]=1;
 
-		for (int i = 0; i < 4; ++i) {
-			if (move[i]) {
-				pair<int, int> newBlue = make_pair(blue.first, blue.second);
-				pair<int, int> newRed = make_pair(red.first, red.second);
-				char newBmap[11][11];
-				memcpy(newBmap, bmap, sizeof(char) * 11 * 11);
-				int a=movingBall(newBmap, newRed, newBlue, i);
-				if (a == 0) {
-					findss(newBmap, newRed, newBlue, dept + 1);
-				}
-				else if (a == 1) {
-					tcheck == false;
-					ans = dept;
-					return;
-				}
+	while(q.size()){
+		int qs=q.size();
+		while(qs){
+			qs--;
+			Node n=q.front();
+			q.pop();
+
+			if(bmap[n.ry][n.rx]=='O'&&bmap[n.ry][n.rx]!=bmap[n.by][n.bx]){
+				ans=dept;
+				return;
 			}
+
+			for(int i=0;i<4;++i){
+				int rx=n.rx;
+				int ry=n.ry;
+				int bx=n.bx;
+				int by=n.by;
+
+				if(bmap[ry+moveY[i]][rx+moveX[i]] !='#'&&bmap[ry][rx]!='O'){
+					ry+=moveY[i];
+					rx+=moveX[i];
+				}
+				if(bmap[by+moveY[i]][bx+moveX[i]]!='#'&&bmap[by][bx]!='O'){
+					by+=moveY[i];
+					bx+=moveX[i];
+				}
+				if(by==ry&&rx==bx){//곂쳤을 경우에
+					if(bmap[by][bx]=='O')continue;
+					if(abs(rx-n.rx)+abs(ry-n.ry)<abs(n.bx-bx)+abs(n.by-by)){//블루가 앞에있을떄
+						by-=moveY[i];
+						bx-=moveX[i];//한칸 앞으로
+					}else{
+						ry-=moveY[i];
+						rx-=moveX[i];
+					}
+				}
+				
+				
+				if(cheking[rx][ry][bx][by])continue;
+				cheking[rx][ry][bx][by]=1;
+				Node emp={rx,ry,bx,by};
+				q.push(emp);
+			}	
 		}
+		if(dept==10){
+				ans=-1;
+				return;
+		}
+		dept++;
 	}
-}
-int* movingCheck(char bmap[11][11], pair<int, int> red) {
-	int move[4] = { 0, };
-	for (int i = 0; i < 4; i++) {
-		if (bmap[red.first + moveY[i]][red.second + moveX[i]] != '#') {
-			move[i] = 1;
-		}
-
-	}
-	return move;
-}
-int movingBall(char bmap[11][11], pair<int, int> red, pair<int, int> blue,int moving) {
-	bool che = true;
-	while (che) {
-		che = false;
-		int movingX = blue.first + moveY[moving];
-		int movingY = blue.second + moveX[moving];
-		char curC = bmap[movingY][movingX];
-
-		if (curC == 'O') {
-			return -1;
-		}
-		else if (curC != '#'&&curC !='R') {
-			bmap[blue.first][blue.second] = '.';
-			bmap[movingY][movingX] = 'B';
-			blue.first += movingY;
-			blue.second += movingX;
-			che = true;
-		}
-
-		movingX = red.first + moveY[moving];
-		movingY = red.second + moveX[moving];
-		curC = bmap[movingY][movingX];
-
-		if (curC == 'O') {
-			return 1;
-		}
-		else if (curC != '#'&&curC != 'R') {
-			bmap[red.first][red.second] = '.';
-			bmap[movingY][movingX] = 'R';
-			red.first += movingY;
-			red.second += movingX;
-			che = true;
-		}
-	}
-	return 0;
+	
 }
