32b8ae09de3a502500fe9e98cac5182de2e062e4
wschoi <52327048+Rogal3@users.noreply.github.com>
2021-01-10 03:28:49 +0900

    Update rogal.java

--- programmers/오픈테스트/rogal.java
+++ programmers/오픈테스트/rogal.java
@@ -1,33 +1,16 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 class Solution {
 	public String[] solution(String[] record) {
 		Map<String, String> nicknameMap = new HashMap<>();
-		record = Arrays.stream(record).filter(r -> {
-			String[] tokens = r.split(" ");
-			switch (tokens[0]) {
-			case "Enter":
-				nicknameMap.put(tokens[1], tokens[2]);
-			case "Leave":
-//				System.out.println(tokens[0] + " " + tokens[1]);
-				return true;
-			case "Change":
-				nicknameMap.put(tokens[1], tokens[2]);
-			}
-			return false;
-		}).toArray(String[]::new);
-		return Arrays.stream(record).map(r -> {
-			String[] tokens = r.split(" ");
-//			System.out.println(tokens[1] + " is " + nicknameMap.get(tokens[1]));
-			switch (tokens[0]) {
-			case "Enter":
-				return nicknameMap.get(tokens[1]) + "님이 들어왔습니다.";
-			case "Leave":
-				return nicknameMap.get(tokens[1]) + "님이 나갔습니다.";
-			}
-			return "";
-		}).toArray(String[]::new);
+		Stream.of(record)
+			.filter(r -> r.startsWith("Enter") || r.startsWith("Change"))
+			.forEach(r -> nicknameMap.put(r.split(" ")[1], r.split(" ")[2]));
+		return Stream.of(record)
+			.filter(r -> r.startsWith("Enter") || r.startsWith("Leave"))
+			.map(r -> nicknameMap.get(r.split(" ")[1]) + (r.split(" ")[0].equals("Enter") ? "님이 들어왔습니다." : "님이 나갔습니다."))
+			.toArray(String[]::new);
 	}
 }
